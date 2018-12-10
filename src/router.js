983e7e5b259dbd694c9989af2f947107291e114d
lyf <liuyufei@czxy.com>
2018-12-10 08:06:17 +0800

    城市管理完

--- src/router.js
+++ src/router.js
@@ -16,6 +16,7 @@ import FormLogin from './pages/form/login';
 import FormRegister from './pages/form/register';
 import Basic from './pages/table/basic';
 import HighTable from './pages/table/highTable';
+import City from './pages/city/index';
 export default class IRouter extends React.Component {
     render() {
         return (
@@ -37,6 +38,7 @@ export default class IRouter extends React.Component {
                                 <Route path="/admin/form/register" component={FormRegister} />
                                 <Route path="/admin/table/basic" component={Basic} />
                                 <Route path="/admin/table/high" component={HighTable} />
+                                <Route path="/admin/city" component={City} />
                                 <Route component={NoMatch} />
                            </Switch>            
                        </Admin>
